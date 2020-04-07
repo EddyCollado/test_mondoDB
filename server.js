5d2a79f8a7f3c69db37497f48d7164d063281278
PhatOrbs <eddcollado@gmail.com>
2020-04-07 15:47:18 -0600

    testing

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URL = process.env.MONGODB_URL || "mongodb://Phatty:Password22!@ds059207.mlab.com:59207/heroku_63q0wsc9";
 
 mongoose.connect(MONGODB_URL);
